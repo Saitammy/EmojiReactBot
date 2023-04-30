8bc1027d5fcf0e76f2d3ad381d6945d812362af8
Saitammy <kishansampreet68@gmail.com>
2023-04-30 20:17:48 +0530

    Removed main.py file to reduce size.

--- bot.py
+++ bot.py
@@ -18,21 +18,26 @@ def run_discord_bot():
     @client.event
     async def on_message(message):
 
-        if message.content == 'pray help':
-            await message.channel.send('Thank you for reaching out to the help section.\n1. pray for sending '
-                                       'image.\n2. pray gif to send random gif.')
+        if message.content == '!p help':
+            await message.channel.send('Thank you for reaching out to the help section.\n1. !p '
+                                       'image.\n2. !pg to send random gif.')
 
         if message.content or message.attachments or message.stickers:
             await message.add_reaction('\U0001F64F')
 
-        if message.content == 'pray':
+        if message.content == '!p':
             with open('Pray.jpg', 'rb') as image:
                 await message.channel.send(file=discord.File(image, 'Pray.jpg'))
 
-        if message.content == 'pray gif':
+        if message.content == '!pg':
             gif = ['1.gif', '2.gif', '3.gif', '4.gif', '5.gif']
             gifed = random.choice(gif)
             with open(gifed, 'rb') as done:
                 await message.channel.send(file=discord.File(done, gifed))
 
+
     client.run(token)
+
+
+if __name__ == '__bot.py__':
+    run_discord_bot()
